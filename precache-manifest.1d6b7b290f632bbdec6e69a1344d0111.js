63a36b10ff6ab25edea6463f564cdc6109dddf22
Jaydip patel <jaydippatel83.pj@gmail.com>
2020-04-04 23:25:33 +0530

    Updates

---
+++ precache-manifest.1d6b7b290f632bbdec6e69a1344d0111.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5455809d294b6312532e89a31e102776",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f46f90898428845bc7f3",
+    "url": "/covid-19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5bf33d105980ce983fde",
+    "url": "/covid-19/static/css/main.66bf9b42.chunk.css"
+  },
+  {
+    "revision": "f46f90898428845bc7f3",
+    "url": "/covid-19/static/js/2.af91c62a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19/static/js/2.af91c62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf33d105980ce983fde",
+    "url": "/covid-19/static/js/main.cc97917e.chunk.js"
+  },
+  {
+    "revision": "eb11f9d0159fb1c3dfc8",
+    "url": "/covid-19/static/js/runtime-main.7db3725e.js"
+  },
+  {
+    "revision": "fa2454ffec979cd1f8ef9a9a5caf9add",
+    "url": "/covid-19/static/media/logo.fa2454ff.webp"
+  }
+]);
